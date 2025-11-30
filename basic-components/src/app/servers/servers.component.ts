a72d6ba76cc8b737f23b4e7fd93b08ecda04e29c
Fatih Maşkaraoğlu <fatihmaskaraoglu@gmail.com>
2025-11-30 18:00:16 +0100

    basic components tries

---
+++ basic-components/src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: app-servers,
+  selector: '[app-servers]',
+  templateUrl: './servers.component.html',
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent implements OnInit{
+
+  constructor(){}
+
+  ngOnInit(): void {
+    
+  }
+
+}
